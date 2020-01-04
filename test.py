54390189684cb6af8ad3b42771a55d1a210450dc
skywu <2458799941@qq.com>
2020-01-04 17:53:04 +0800

    change request network address

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
-return_str = requests.get('http://localhost:5000/')
+return_str = requests.get('http://0.0.0.0:5000/')
 return_str.encoding = 'utf-8' 
 
 is_pass = 'Hello' in return_str.text
